4372e174e1a389d593bf316d9b68913dfeb1b11b
Sofia Blom <46625752+s02blom@users.noreply.github.com>
2024-04-09 10:16:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# python-sql-devcontainer
+This repository contains a dev container with a MySQL server and python. 
+
+## Env-file
+The `.env` file should be located inside [`.devcontainer`](./.devcontainer/). An example of an appripriate .env file:
+```
+DATABASE_HOST=mysql_db
+DATABASE_USER=ROOT
+DATABASE_PASSWORD=my-password
+DATABASE_DB=my-database
+DATABASE_PORT=3306
+```
+
---
+++ python/Connector.py
@@ -0,0 +1,30 @@
+import os
+import mysql.connector as Kalle
+
+
+connection = Kalle.connect( host = os.environ.get('DATABASE_HOST'),
+                            user = os.environ.get('DATABASE_USER'),
+                            password = os.environ.get('DATABASE_PASSWORD'),
+                            database = os.environ.get('DATABASE_DB'),
+                            port = os.environ.get('DATABASE_PORT'))
+
+with connection:
+    with connection.cursor() as cursor:
+        sql = "SHOW DATABASES;"
+        cursor.execute(sql)
+        print(cursor.fetchall())
+        #sql = "USE example;"
+        #cursor.execute(sql)
+        sql = "SHOW TABLES;"
+        cursor.execute(sql)
+        print(cursor.fetchall())
+        sql = """CREATE TABLE IF NOT EXISTS guestbook (
+            id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            visitor_name VARCHAR(255) NOT NULL,
+            note TEXT NULL,
+            created_at DATETIME DEFAULT now()
+            );"""
+        cursor.execute(sql)
+        sql = "SHOW TABLES;"
+        cursor.execute(sql)
+        print(cursor.fetchall())
