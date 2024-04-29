b582b7021797d0f0a3fb1e4ac7e09d22fad940b1
Sofia BLom - Ender <sofia@familjenblom.se>
2024-04-29 14:55:45 +0200

    Return to working without g, from flask

--- LegoWebStore/app/db.py
+++ LegoWebStore/app/db.py
@@ -1,6 +1,8 @@
 import os
 import mysql.connector as Kalle
 from mysql.connector import errorcode
+from flask import current_app, g
+import click
 
 
 #connection = Kalle.connect( host = os.environ.get('DATABASE_HOST'),
@@ -10,21 +12,20 @@ from mysql.connector import errorcode
 #                           port = os.environ.get('DATABASE_PORT'))
 
 def get_connection():
-    if "db" not in g:
-        try:
-            g.db = Kalle.connect( host = os.environ.get('DATABASE_HOST'),
-                                    user = os.environ.get('DATABASE_USER'),
-                                    password = os.environ.get('DATABASE_PASSWORD'),
-                                    database = os.environ.get('DATABASE_DB'),
-                                    port = os.environ.get('DATABASE_PORT'))
-        except Kalle.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password") #Ought to be replaced with some logging tool?
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-    return g.db
+    try:
+        connection = Kalle.connect( host = os.environ.get('DATABASE_HOST'),
+                                user = os.environ.get('DATABASE_USER'),
+                                password = os.environ.get('DATABASE_PASSWORD'),
+                                database = os.environ.get('DATABASE_DB'),
+                                port = os.environ.get('DATABASE_PORT'))
+    except Kalle.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password") #Ought to be replaced with some logging tool?
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return connection
 
 def close_connection(connection):
     connection.close()
