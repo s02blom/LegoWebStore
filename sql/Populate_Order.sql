3b805787f6389a71d7397f9f41a5daf3dbe8e2ae
Sofia BLom - Ender <sofia@familjenblom.se>
2024-05-14 11:55:02 +0200

    Add Orders

---
+++ sql/Populate_Order.sql
@@ -0,0 +1,8 @@
+INSERT INTO `Order` (OrderDate, ShippingDate, ArrivalDate, Customer, ShippingAdress) VALUES -- Have a default and and a trigger to automatically keep total sum up to date?
+("2022-03-04", "2022-03-10", "2022-03-15", 1, 2),
+("2022-02-25", "2022-03-01", "2022-03-03", 1, 7),
+("2022-03-15", "2022-03-18", "2022-03-22", 2, 3),
+("2022-03-04", "2022-03-30", "2022-04-03", 3, 6),
+("2022-03-01", "2022-03-02", "2022-03-04", 4, 4),
+("2022-03-15", "2022-03-16", "2022-03-20", 5, 1)
+;
