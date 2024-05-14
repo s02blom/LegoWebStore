78073ebb4f01b2246762a0b714619ed4a1792b2d
Sofia BLom - Ender <sofia@familjenblom.se>
2024-05-14 10:23:55 +0200

    Add Shipping Adresses

---
+++ sql/Populate_ShippingAdress.sql
@@ -0,0 +1,8 @@
+INSERT INTO ShippingAdress (StreetAdress, PostCode, City, Country) VALUES
+("Jane street 15", "TW5B", "London", "UK"),
+("Queen street 1", "1504", "Copenhagen", "DK"),
+("Lyon dé trivo 6", "43115", "Lyon", "FR"),
+("Louvre", "75001", "Paris", "FR"),
+("Av. Gustave Eiffel", "75007", "Paris", "FR"),
+("Domkloster 4", "50667", "Köln", "DE")
+;
