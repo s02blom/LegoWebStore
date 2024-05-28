15e2efbc3214c20fcac9676c11da8b5a168eb75b
Sofia BLom - Ender <sofia@familjenblom.se>
2024-05-28 12:24:26 +0200

    We no longer have SQL syntax error's
    But we still are unable to exevute the command due to lack of privileges

--- sql/Triggers.sql
+++ sql/Triggers.sql
@@ -5,8 +5,8 @@ CREATE TRIGGER SumOrder BEFORE INSERT ON OrderContent
 FOR EACH ROW 
 BEGIN    
     DECLARE sum, price INT;
-    SET sum = SELECT totalSum FROM `Order` WHERE New.Order = `Order`.ID;
-    SET price = SELECT Price FROM LegoSet  WHERE New.LegoSet = LegoSet.ID;
+    SELECT totalSum INTO sum FROM `Order` WHERE New.`Order` = `Order`.ID;
+    SELECT Price INTO price FROM LegoSet  WHERE New.LegoSet = LegoSet.ID;
     SET @totalSum = sum + price * New.Quantity;
 END ??
 
