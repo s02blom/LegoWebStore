4ffa6c00d33e836e6898ab005c68a970525bbefc
Sofia BLom - Ender <sofia@familjenblom.se>
2024-05-29 14:56:42 +0200

    Something that should work, but does not :(

--- LegoWebStore/app/sql/Triggers.sql
+++ LegoWebStore/app/sql/Triggers.sql
@@ -1,15 +1,16 @@
 DROP TRIGGER IF EXISTS SumOrder;
 
-DELIMITER ??
+DELIMITER $$
 CREATE TRIGGER SumOrder BEFORE INSERT ON OrderContent
 FOR EACH ROW 
 BEGIN    
-    DECLARE sum, price INT;
-    SELECT totalSum INTO sum FROM `Order` WHERE New.`Order` = `Order`.ID;
-    SELECT Price INTO price FROM LegoSet  WHERE New.LegoSet = LegoSet.ID;
-    UPDATE OrderContent set totalSum = sum + price * new.Quantity
-    WHERE `Order`.ID = New.`Order`;
+    DECLARE oldValue, pricePerKit INT;
+    SELECT totalSum INTO oldValue FROM `Order` WHERE New.`Order` = `Order`.ID;
+    SELECT Price INTO pricePerKit FROM LegoSet  WHERE New.LegoSet = LegoSet.ID;
+    UPDATE `Order`
+		SET TotalSum = oldValue + pricePerKit * new.Quantity
+		WHERE `Order`.ID = New.`Order`;
     
-END ??
+END $$
 
 DELIMITER ;
