fdb72257d743d0fca601ba44e68a905d594f9a0b
Sofia BLom - Ender <sofia@familjenblom.se>
2024-07-29 12:32:07 +0200

    Base of a form

--- LegoWebStore/app/frontpage.py
+++ LegoWebStore/app/frontpage.py
@@ -1,6 +1,5 @@
 from flask import (Blueprint, flash, g, render_template, request, url_for)
 from . import db
-#from db import get_connection
 
 blueprint = Blueprint('frontpage', __name__)
 
@@ -9,7 +8,14 @@ blueprint = Blueprint('frontpage', __name__)
 def index():
     print("Found / and index")
     connection = db.get_connection()
+    unavilable_sets = []
     with connection.cursor() as cursor:
         """Get things from server"""
-        pass
-    return render_template("frontpage.html")
+        cursor.execute("SELECT * from LegoSet")
+        avilable_sets = cursor.fetchall()
+    return render_template("frontpage.html", avilable_sets=avilable_sets, unavilable_sets=unavilable_sets)
+
+@blueprint.route("/orders", methods=("POST",))
+def order_lego_sets():
+    print("Found endpoint /orders")
+    pass
