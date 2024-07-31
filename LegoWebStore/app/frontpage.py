0da85c62fb0e6f792d7022b77e693b2739d6511f
Sofia BLom - Ender <sofia@familjenblom.se>
2024-07-31 13:58:07 +0200

    Add form to frontpage
    No functionality yet

--- LegoWebStore/app/frontpage.py
+++ LegoWebStore/app/frontpage.py
@@ -7,32 +7,31 @@ from . import db
 blueprint = Blueprint('frontpage', __name__)
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=("GET", "POST"))
 def index():
     print("Found / and index")
     connection = db.get_connection()
     unavilable_sets = []
+    new_order = NewOrder()
+    if new_order.validate_on_submit():
+        print(f"Company name: {new_order.company_name.data}")
+        pass
     with connection.cursor() as cursor:
         """Get things from server"""
         cursor.execute("SELECT * from LegoSet")
         avilable_sets = cursor.fetchall()
-    return render_template("frontpage.html", avilable_sets=avilable_sets, unavilable_sets=unavilable_sets)
+    return render_template("frontpage.html", avilable_sets=avilable_sets, unavilable_sets=unavilable_sets, form=new_order)
 
 class NewOrder(FlaskForm):
     company_name = StringField("Company name", validators=[DataRequired()])
     company_country = StringField("International two letter code of your country", validators=[DataRequired()])
-    email = StringField("email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
     street_adress = StringField("Street adress", validators=[DataRequired()])
     post_code = StringField("Post code", validators=[DataRequired()])
     city = StringField("City", validators=[DataRequired()])
     shipping_country = StringField("International two letter code of your country", validators=[DataRequired()])
 
-    lego_sets = FieldList(IntegerField("Lego set id", validators=[DataRequired()]), 
-                          IntegerField("Quantity", validators=[DataRequired()]), 
+    lego_id = FieldList(IntegerField("Lego set id", validators=[DataRequired()]), 
                           min_entries=1, max_entries=100)
-
-
-@blueprint.route('/order', methods=("POST",))
-def order():
-    
-    pass
+    lego_quantity = FieldList(IntegerField("Quantity", validators=[DataRequired()]),
+                              min_entries=1, max_entries=100)
