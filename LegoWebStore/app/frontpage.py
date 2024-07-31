4509f3faf042cf89f9bdda0ac744a095dae592b6
Sofia BLom - Ender <sofia@familjenblom.se>
2024-07-31 13:09:41 +0200

    Add class for handeling new orders

--- LegoWebStore/app/frontpage.py
+++ LegoWebStore/app/frontpage.py
@@ -1,4 +1,7 @@
 from flask import (Blueprint, flash, g, render_template, request, url_for)
+from flask_wtf import FlaskForm
+from wtforms import StringField, FieldList, IntegerField
+from wtforms.validators import DataRequired
 from . import db
 
 blueprint = Blueprint('frontpage', __name__)
@@ -15,7 +18,21 @@ def index():
         avilable_sets = cursor.fetchall()
     return render_template("frontpage.html", avilable_sets=avilable_sets, unavilable_sets=unavilable_sets)
 
-@blueprint.route("/orders", methods=("POST",))
-def order_lego_sets():
-    print("Found endpoint /orders")
+class NewOrder(FlaskForm):
+    company_name = StringField("Company name", validators=[DataRequired()])
+    company_country = StringField("International two letter code of your country", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired()])
+    street_adress = StringField("Street adress", validators=[DataRequired()])
+    post_code = StringField("Post code", validators=[DataRequired()])
+    city = StringField("City", validators=[DataRequired()])
+    shipping_country = StringField("International two letter code of your country", validators=[DataRequired()])
+
+    lego_sets = FieldList(IntegerField("Lego set id", validators=[DataRequired()]), 
+                          IntegerField("Quantity", validators=[DataRequired()]), 
+                          min_entries=1, max_entries=100)
+
+
+@blueprint.route('/order', methods=("POST",))
+def order():
+    
     pass
