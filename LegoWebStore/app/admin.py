cd4a4620bfbf38181205969d8c33ff1ff456da6b
Sofia BLom - Ender <sofia@familjenblom.se>
2024-08-09 12:24:05 +0200

    Add comment about some thoughts

--- LegoWebStore/app/admin.py
+++ LegoWebStore/app/admin.py
@@ -6,4 +6,8 @@ blueprint = Blueprint('admin', __name__)
 @blueprint.route("/admin", methods=("GET", "POST"))
 def index():
     print("Found admins page")
+    # I need a data set that contains All the data about and order, with
+    # a sublist of information about the lego sets themselves. Unfortunatly I
+    # belive it has to be split into two different sql statements to get the right 
+    # structure in Python. 
     return render_template("admin.html")
