b623a8a7f7183dabf8336a1426e9b75095d4e7d2
Sofia BLom - Ender <sofia@familjenblom.se>
2024-08-20 10:25:59 +0200

    Add trigger RemoveLegoBricks
    Untested, for removing lego brick in storage when a new order comes in

--- LegoWebStore/app/sql/Triggers.sql
+++ LegoWebStore/app/sql/Triggers.sql
@@ -1,4 +1,4 @@
-DROP TRIGGER IF EXISTS SumOrder;
+DROP TRIGGER IF EXISTS SumOrder, RemoveLegoBricks;
 
 CREATE TRIGGER SumOrder BEFORE INSERT ON OrderContent
 FOR EACH ROW 
@@ -10,4 +10,29 @@ BEGIN
 		SET TotalSum = oldValue + pricePerKit * new.Quantity
 		WHERE `Order`.ID = New.`Order`;
     
+END
+
+CREATE TRIGGER RemoveLegoBricks BEFORE INSERT ON OrderContent
+FOR EACH ROW
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cursorSetContent FOR SELECT * FROM LegoSetContent WHERE LegoSet = New.LegoSet;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    DECLARE storageID, brickQuantity INT;
+    DECLARE lSet, lBrick, lQuant INT;
+
+    OPEN cursorSetContent;
+
+    loopy: LOOP
+      FETCH cursorSetContent into lSet, lBrick, lQuant;
+      IF done THEN
+        LEAVE loopy;
+      END IF;
+      SELECT StorageLocation INTO storageID FROM LegoBrick WHERE ID = lBrick;
+      SELECT Quantity into brickQuantity FROM StorageLocation WHERE ID = storageID;
+      UPDATE StorageLocation
+        SET Quantity = brickQuantity - lQuant*New.Quantity
+        WHERE StorageLocation.id = storageID;
+    END LOOP;
+    CLOSE cursorSetContent;
 END
